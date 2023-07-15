d8d91096e6cd420bbcaccb94c388991bfd476ac1
mickeybeurskens <mickeybeurskens@gmail.com>
2023-07-15 23:17:07 +0200

    Add tests for utils

--- streamlitapp/perry/users.py
+++ streamlitapp/perry/users.py
@@ -1,5 +1,6 @@
 import pydantic
 import pathlib
+from perry.utils import save_pydantic_instance, load_pydantic_instance
 
 
 class User(pydantic.BaseModel):
@@ -15,3 +16,18 @@ def get_user_save_dir() -> pathlib.Path:
     if not path.exists():
         path.mkdir(parents=True)
     return path
+
+
+def save_user(user: User, path: pathlib.Path) -> None:
+    """Save a user to a json file."""
+    file_name = f"user_{user.username}.json"
+    file_path = path / file_name
+    save_pydantic_instance(user, file_path)
+
+
+def load_user(username: str, path: pathlib.Path) -> User:
+    """Load a user from a json file."""
+    file_name = f"user_{username}.json"
+    file_path = path / file_name
+    user = load_pydantic_instance(User, file_path)
+    return user
---
+++ streamlitapp/tests/test_utils.py
@@ -0,0 +1,28 @@
+import pytest 
+import pydantic
+import tempfile
+from perry.utils import *
+
+
+class TestModel(pydantic.BaseModel):
+    name: str
+    age: int
+
+
+def test_save_should_output_json_file():
+    """ Saving a pydantic model should create a json file. """
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        tmp_dir = pathlib.Path(tmp_dir)
+        model = TestModel(name="John", age=42)
+        save_pydantic_instance(model, tmp_dir / "test.json")
+        assert (tmp_dir / "test.json").exists()
+
+
+def test_load_should_equal_save():
+    """ Loading a pydantic model should equal the original model. """
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        tmp_dir = pathlib.Path(tmp_dir)
+        model = TestModel(name="John", age=42)
+        save_pydantic_instance(model, tmp_dir / "test.json")
+        loaded_model = load_pydantic_instance(TestModel, tmp_dir / "test.json")
+        assert model == loaded_model
