1acd40fa4df5235a8b00fde48343a8242e742303
mickeybeurskens <mickeybeurskens@gmail.com>
2023-09-26 01:10:13 +0200

    Use fastapi dependencies to inject database into api endpoints

--- backend/perry/api/endpoints/document.py
+++ backend/perry/api/endpoints/document.py
@@ -1,5 +1,6 @@
 from typing import Annotated
 from fastapi import APIRouter, Depends, status, HTTPException, UploadFile
+from sqlalchemy.orm import Session
 from perry.api.authentication import get_current_user_id
 from perry.api.schemas import APIDocument
 from perry.db.operations.documents import (
@@ -11,7 +12,7 @@ from perry.db.operations.documents import (
     document_owned_by_user,
     get_user_documents,
 )
-from perry.db.session import DatabaseSessionManager as DSM
+from perry.api.dependencies import get_db
 
 
 file_router = APIRouter()
@@ -22,6 +23,7 @@ document_router = APIRouter()
 async def upload_file(
     file: UploadFile,
     db_user_id: Annotated[int, Depends(get_current_user_id)],
+    db: Session = Depends(get_db),
 ):
     if not file.content_type == "application/pdf":
         raise HTTPException(
@@ -29,18 +31,16 @@ async def upload_file(
             detail="File must be a PDF",
         )
     try:
-        doc_id = save_file(DSM.get_db_session, file, ".pdf")
+        doc_id = save_file(db, file, ".pdf")
     except Exception as e:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail="Could not save file",
         )
     try:
-        update_document(
-            DSM.get_db_session, doc_id, title=file.filename, user_ids=[db_user_id]
-        )
+        update_document(db, doc_id, title=file.filename, user_ids=[db_user_id])
     except Exception as e:
-        remove_file(DSM.get_db_session, doc_id)
+        remove_file(db, doc_id)
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail="Could not update document",
@@ -50,15 +50,17 @@ async def upload_file(
 
 @file_router.delete("/{document_id}", status_code=status.HTTP_200_OK)
 async def delete_file(
-    document_id: int, db_user_id: Annotated[int, Depends(get_current_user_id)]
+    document_id: int,
+    db_user_id: Annotated[int, Depends(get_current_user_id)],
+    db: Session = Depends(get_db),
 ):
-    if not document_owned_by_user(DSM.get_db_session, document_id, db_user_id):
+    if not document_owned_by_user(db, document_id, db_user_id):
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
             detail="Not authorized to delete document",
         )
     try:
-        remove_file(DSM.get_db_session, document_id)
+        remove_file(db, document_id)
     except Exception as e:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
@@ -70,20 +72,21 @@ async def delete_file(
 async def retrieve_file_binary(
     document_id: int,
     db_user_id: Annotated[int, Depends(get_current_user_id)],
+    db: Session = Depends(get_db),
 ):
-    db_doc = get_document(DSM.get_db_session, document_id)
+    db_doc = get_document(db, document_id)
     if not db_doc:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="Document not found",
         )
-    if not document_owned_by_user(DSM.get_db_session, document_id, db_user_id):
+    if not document_owned_by_user(db, document_id, db_user_id):
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
             detail="Not authorized to get document",
         )
     try:
-        file_bytes = load_file(DSM.get_db_session, document_id)
+        file_bytes = load_file(db, document_id)
     except Exception as e:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
@@ -94,15 +97,17 @@ async def retrieve_file_binary(
 
 @document_router.get("/{document_id}", response_model=APIDocument)
 async def get_doc_by_id(
-    document_id: int, db_user_id: Annotated[int, Depends(get_current_user_id)]
+    document_id: int,
+    db_user_id: Annotated[int, Depends(get_current_user_id)],
+    db: Session = Depends(get_db),
 ):
-    doc = get_document(DSM.get_db_session, document_id)
+    doc = get_document(db, document_id)
     if not doc:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="Document not found",
         )
-    if not document_owned_by_user(DSM.get_db_session, document_id, db_user_id):
+    if not document_owned_by_user(db, document_id, db_user_id):
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
             detail="Not authorized to get document",
@@ -111,8 +116,11 @@ async def get_doc_by_id(
 
 
 @document_router.get("/", response_model=list[APIDocument])
-async def get_all_docs(db_user_id: Annotated[int, Depends(get_current_user_id)]):
-    db_documents = get_user_documents(DSM.get_db_session, db_user_id)
+async def get_all_docs(
+    db_user_id: Annotated[int, Depends(get_current_user_id)],
+    db: Session = Depends(get_db),
+):
+    db_documents = get_user_documents(db, db_user_id)
     docs = []
     for doc in db_documents:
         docs.append(APIDocument(title=doc.title, id=doc.id))
