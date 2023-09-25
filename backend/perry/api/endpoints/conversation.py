98332f47dd50a932613e3155d8a69058302c3e1b
mickeybeurskens <mickeybeurskens@gmail.com>
2023-09-25 16:26:02 +0200

    Create conversations through the API

--- backend/perry/api/endpoints/conversation.py
+++ backend/perry/api/endpoints/conversation.py
@@ -5,6 +5,7 @@ from perry.api.schemas import APIDocument
 from perry.db.operations.documents import (
     get_document,
     get_user_documents,
+    update_document,
 )
 from perry.db.operations.conversations import (
     create_conversation,
@@ -19,6 +20,8 @@ from perry.db.operations.agents import (
     update_agent,
 )
 from perry.db.session import DatabaseSessionManager as DSM
+from perry.agents.manager import AgentManager
+from perry.agents.base import AgentRegistry
 
 
 conversation_router = APIRouter()
@@ -35,32 +38,63 @@ class ConversationConfig(BaseModel):
 
 
 @conversation_router.post("/", status_code=status.HTTP_201_CREATED)
-async def create_conversation(
+async def conversation_agent_setup(
     conversation_config: ConversationConfig,
     db_user_id: Annotated[int, Depends(get_current_user_id)],
 ):
     db = DSM.get_db_session
     # check doc ids
-    user_document_ids = [doc.id for doc in get_user_documents(db, db_user_id)]
+    docs = get_user_documents(db, db_user_id)
+    user_document_ids = [doc.id for doc in docs]
     if not set(conversation_config.doc_ids).issubset(set(user_document_ids)):
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Invalid document ids provided.",
         )
+
     # create agent
-    # create conversation
-    # return conversation id
+    # check type in the agent and return a class instance
+    try:
+        agent_class = AgentRegistry.get_agent_class(conversation_config.agent_type)
+    except Exception:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Invalid agent type provided.",
+        )
+
+    try:
+        agent = agent_class(db, conversation_config.agent_settings)
+    except Exception:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Invalid agent settings provided.",
+        )
+
+    conversation_id = create_conversation()
+    try:
+        update_conversation(db, conversation_id, user_id=db_user_id)
+        update_agent(db, agent, conversation_id=conversation_id)
+        for doc in docs:
+            conv_ids = [conv.id for conv in doc.conversations]
+            update_document(db, doc.id, conversation_id=conv_ids + [conversation_id])
+    except Exception:
+        remove_conversation(db, conversation_id)
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Could not create conversation.",
+        )
+    return conversation_id
 
 
 @conversation_router.get("/{conversation_id}", status_code=status.HTTP_200_OK)
-async def get_conversation(
+async def get_conversation_info(
     conversation_id: int, db_user_id: Annotated[int, Depends(get_current_user_id)]
 ):
     pass
 
 
 @conversation_router.delete("/{conversation_id}", status_code=status.HTTP_200_OK)
-async def delete_conversation(
+async def remove_conversation(
     conversation_id: int, db_user_id: Annotated[int, Depends(get_current_user_id)]
 ):
     pass
--- backend/tests/api/endpoints/test_conversation.py
+++ backend/tests/api/endpoints/test_conversation.py
@@ -28,12 +28,52 @@ def str_path_conv_endpoint() -> str:
 
 
 @pytest.fixture(scope="function")
-def create_conversation_test_client(test_client, monkeypatch):
+def create_conversation_mock(test_client, monkeypatch):
     test_client.app.dependency_overrides[
         get_current_user_id
     ] = lambda: get_test_user_id()
     monkeypatch.setattr(str_path_conv_endpoint() + ".create_conversation", lambda: True)
-    yield test_client
+    mock_get_user_documents = Mock(
+        return_value=[
+            Mock(id=1, conversations=[]),
+            Mock(id=2, conversations=[]),
+            Mock(id=3, conversations=[]),
+        ]
+    )
+    monkeypatch.setattr(
+        str_path_conv_endpoint() + ".get_user_documents", mock_get_user_documents
+    )
+    mock_agent = Mock()
+    mock_agent_constructor = Mock(return_value=mock_agent)
+    monkeypatch.setattr(
+        str_path_conv_endpoint() + ".AgentRegistry.get_agent_class",
+        mock_agent_constructor,
+    )
+
+    conversation_json = get_create_conversation_json()
+    conversation_json["agent_settings"] = {"invalid_agent_settings": True}
+
+    mock_update_conversation = Mock(return_value=True)
+    monkeypatch.setattr(
+        str_path_conv_endpoint() + ".update_conversation", mock_update_conversation
+    )
+    mock_update_agent = Mock(return_value=True)
+    monkeypatch.setattr(str_path_conv_endpoint() + ".update_agent", mock_update_agent)
+    mock_update_document = Mock(return_value=True)
+    monkeypatch.setattr(
+        str_path_conv_endpoint() + ".update_document", mock_update_document
+    )
+
+    yield {
+        "test_client": test_client,
+        "mock_agent": mock_agent,
+        "mock_agent_constructor": mock_agent_constructor,
+        "mock_get_user_documents": mock_get_user_documents,
+        "conversation_json": conversation_json,
+        "mock_update_conversation": mock_update_conversation,
+        "mock_update_agent": mock_update_agent,
+        "mock_update_document": mock_update_document,
+    }
 
 
 @pytest.mark.parametrize(
@@ -52,25 +92,115 @@ def test_endpoints_should_refuse_non_authenticated_users(
     assert response.status_code == status_code
 
 
-def test_create_conversation_errors_with_unowned_doc_ids(
-    create_conversation_test_client, monkeypatch
-):
-    valid_doc_ids = [1, 2, 3]
-    invalid_doc_ids = valid_doc_ids + [4]
-    mock_get_user_documents = Mock(
-        return_value=[Mock(id=doc_id) for doc_id in valid_doc_ids]
-    )
-    monkeypatch.setattr(
-        str_path_conv_endpoint() + ".get_user_documents", mock_get_user_documents
-    )
+def test_create_conversation_errors_with_unowned_doc_ids(create_conversation_mock):
+    test_client = create_conversation_mock["test_client"]
+    mock_get_user_documents = create_conversation_mock["mock_get_user_documents"]
+    invalid_doc_ids = [-1]
 
     conversation_json = get_create_conversation_json()
     conversation_json["doc_ids"] = invalid_doc_ids
 
-    response = create_conversation_test_client.post(
+    response = test_client.post(
         CONVERSATION_URL + "/",
         json=conversation_json,
     )
     assert response.status_code == status.HTTP_400_BAD_REQUEST
     assert response.json() == {"detail": "Invalid document ids provided."}
     assert mock_get_user_documents.call_count == 1
+
+
+def test_create_conversation_errors_with_invalid_agent_type(create_conversation_mock):
+    test_client = create_conversation_mock["test_client"]
+    mock_get_user_documents = create_conversation_mock["mock_get_user_documents"]
+    mock_agent_constructor = create_conversation_mock["mock_agent_constructor"]
+    conversation_json = create_conversation_mock["conversation_json"]
+
+    conversation_json["agent_type"] = "invalid_agent_type"
+    mock_agent_constructor.side_effect = KeyError("Invalid agent type")
+
+    response = test_client.post(
+        CONVERSATION_URL + "/",
+        json=conversation_json,
+    )
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert response.json() == {"detail": "Invalid agent type provided."}
+    assert mock_get_user_documents.call_count == 1
+
+
+def test_create_conversation_errors_with_invalid_agent_settings(
+    create_conversation_mock,
+):
+    test_client = create_conversation_mock["test_client"]
+    mock_get_user_documents = create_conversation_mock["mock_get_user_documents"]
+    conversation_json = create_conversation_mock["conversation_json"]
+    mock_agent = create_conversation_mock["mock_agent"]
+
+    mock_agent.side_effect = KeyError("Invalid agent settings")
+    conversation_json["agent_settings"] = {"invalid_agent_settings": True}
+
+    response = test_client.post(
+        CONVERSATION_URL + "/",
+        json=conversation_json,
+    )
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert response.json() == {"detail": "Invalid agent settings provided."}
+    assert mock_get_user_documents.call_count == 1
+
+
+@pytest.mark.parametrize(
+    "method, exception",
+    [
+        ("update_conversation", KeyError("Invalid conversation")),
+        ("update_agent", KeyError("Invalid agent")),
+        ("update_document", KeyError("Invalid document")),
+    ],
+)
+def test_create_conversation_errors_on_db_exceptions(
+    create_conversation_mock, method, exception, monkeypatch
+):
+    test_client = create_conversation_mock["test_client"]
+
+    monkeypatch.setattr(
+        str_path_conv_endpoint() + f".{method}", Mock(side_effect=exception)
+    )
+
+    response = test_client.post(
+        CONVERSATION_URL + "/",
+        json=create_conversation_mock["conversation_json"],
+    )
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert response.json() == {"detail": "Could not create conversation."}
+
+
+def test_create_conversation_succeeds(create_conversation_mock):
+    response = create_conversation_mock["test_client"].post(
+        CONVERSATION_URL + "/",
+        json=create_conversation_mock["conversation_json"],
+    )
+
+    assert response.status_code == status.HTTP_201_CREATED
+    assert create_conversation_mock["mock_get_user_documents"].call_count == 1
+    assert create_conversation_mock["mock_agent_constructor"].call_count == 1
+    assert create_conversation_mock["mock_agent"].call_count == 1
+    assert create_conversation_mock["mock_update_conversation"].call_count == 1
+    assert create_conversation_mock["mock_update_agent"].call_count == 1
+    assert create_conversation_mock["mock_update_document"].call_count == 3
+
+
+def test_create_conversation_updates_document_with_conversation_id(
+    create_conversation_mock,
+):
+    test_client = create_conversation_mock["test_client"]
+    mock_update_document = create_conversation_mock["mock_update_document"]
+    mock_update_document.side_effect = lambda db, doc_id, **kwargs: doc_id
+
+    response = test_client.post(
+        CONVERSATION_URL + "/",
+        json=create_conversation_mock["conversation_json"],
+    )
+
+    assert response.status_code == status.HTTP_201_CREATED
+    assert mock_update_document.call_count == 3
+    assert mock_update_document.call_args_list[0][1]["conversation_id"] == [1]
+    assert mock_update_document.call_args_list[1][1]["conversation_id"] == [1]
+    assert mock_update_document.call_args_list[2][1]["conversation_id"] == [1]
